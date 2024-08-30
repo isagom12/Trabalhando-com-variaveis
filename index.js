d2a38e6b566ed7a74741bc7799830a9cb930eac7
isa gomes <161726701+isagom12@users.noreply.github.com>
2024-08-30 01:26:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log (`Trabalhando com listas`);
+
+console.log ('Cidades que ja visitei');
+
+const curitiba = `Curitiba`;
+const SaoPaulo = `São Paulo`;
+const SantaCatarina = `Santa Catarina`;
+const morretes = `Morretes`;
+const MinasGerais = `Minas Gerais`;
+
+console.log (curitiba, SaoPaulo, SantaCatarina, morretes, MinasGerais);
+
+const cidadesQueJaVisitei = new Array(
+`Curitiba`
+`São Paulo`
+`Santa Catarina`
+`Morretes`
+`Minas Gerais`
+);
+
+console.log(cidadesQueJaVisitei);
+
+
